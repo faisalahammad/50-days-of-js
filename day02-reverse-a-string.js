a64318983b165dfc10936af21e25e774eba0c605
Faisal Ahammad <faisalahammad24@gmail.com>
2022-01-01 13:48:59 +0600

    DONE - Day 03: Reverse a number

--- day02-reverse-a-string.js
+++
@@ -1,7 +0,0 @@
-const str = "JavaScript is awesome"
-
-function reverseAString(str) {
-    return (str === '') ? '' : reverseAString(str.substr(1)) + str.charAt(0);
-}
-
-console.log(`Reversed string is: ${reverseAString(str)}`)
